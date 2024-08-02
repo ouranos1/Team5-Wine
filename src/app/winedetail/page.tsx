40c1d45640cac87a34a08e8cdeb1ee9e642ba1cd
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-02 20:26:41 +0900

    예시 축소 (바보 이슈)

--- src/app/winedetail/page.tsx
+++ src/app/winedetail/page.tsx
@@ -5,8 +5,8 @@ const DropDownPage = () => {
     const items = [
         { name: "마이페이지", path: "/myprofile" },
         { name: "로그아웃", path: "" },
-        { name: "안뇽하삽니까", path: "" },
-        { name: "똑똑히 봤슈", path: "" }
+        // { name: "안뇽하삽니까", path: "" },
+        // { name: "똑똑히 봤슈", path: "" }
     ];
 
     return (
