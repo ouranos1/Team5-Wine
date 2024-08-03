3b36e0a05c1465cc4fa25f63d4c68d43eb169ac0
yeram7591 <dhflgjsxj2@naver.com>
2024-08-03 12:51:22 +0900

    feat:소셜 로그인 버튼 구현

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import OAuthButton from '@/components/oauthbuttoncomponent/OAuthButton';
 import SearchBar from '@/components/searchbarcomponent/SearchBar';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import googleLogo from '@/assets/googleLogo.svg';
+import kakaoLogo from '@/assets/kakaoLogo.svg';
+import naverLogo from '@/assets/naverLogo.svg';
 
 export default function SignUp() {
+  const { data: session } = useSession();
+
   return (
     <>
       <SearchBar placeholder="와인을 검색해 보세요" />
+      <OAuthButton logo={googleLogo} text={session ? '로그아웃' : 'google로 시작하기'} onClick={session ? signOut : () => signIn('google')} />
     </>
   );
 }
