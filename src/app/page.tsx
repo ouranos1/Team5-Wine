703e0fef37fa60b119ec28fc07ddb183b89014d8
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-04 19:34:16 +0900

    파일 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import "@/styles/Font-Mixins.scss";
 export default function Home() {
   return (
     <div>
-      
+
     </div>
   );
 }
--- src/components/dropdown/사용방법.tsx
+++ src/components/dropdown/사용방법.tsx
@@ -4,7 +4,7 @@ import Dropdown from '../../components/dropdown/DropDown';
 const DropDownPage = () => {
     const items = [
         { name: "마이페이지", path: "/myprofile" },
-        { name: "로그아웃", path: "" },
+        { name: "로그아웃", path: "/" },
         // { name: "안뇽하삽니까", path: "" },
         // { name: "똑똑히 봤슈", path: "" }
     ];
