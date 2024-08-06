15b915412abe8f1a6ead653dde53c57c270cb6ce
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-06 13:57:53 +0900

    쓸데없는 코드 삭제

--- src/app/winedetail/page.tsx
+++ src/app/winedetail/page.tsx
@@ -1,37 +1,5 @@
 "use client";
 
-// import React from 'react';
-// import Card from '../../components/cardSoohyun/Card';
-// import useFetchData from '../../components/cardSoohyun/useFetchData';
-
-// interface ApiResponse {
-//     imageUrl: string;
-//     avgRating: number;
-//     description: string;
-// }
-
-// const App: React.FC = () => {
-//     const { data, loading, error } = useFetchData<ApiResponse>('/v1/wine', '와인 데이터');
-
-//     if (loading) return <div>로딩 중...</div>;
-//     if (error) return <div>오류 발생: {error.message}</div>;
-
-//     return (
-//         <div>
-//             {data && (
-//                 <Card
-//                     imageUrl={data.imageUrl}
-//                     avgRating={data.rating}
-//                     description={data.description}
-//                 />
-//             )}
-//         </div>
-//     );
-// };
-
-// export default App;
-
-
 import React from 'react';
 import Cardmonthly from '../../components/cardmonthly/Card';
 import Cardmy from '../../components/cardmy/Card';
