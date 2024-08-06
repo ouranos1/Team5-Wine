80c0f8473338ddc3bc371076505940b935713b8f
cossgii <eastar385@naver.com>
2024-08-06 19:45:51 +0900

    fix: 인풋 수정

--- src/components/inputComponent/Input.tsx
+++ src/components/inputComponent/Input.tsx
@@ -6,24 +6,20 @@ import './Input.scss';
 interface Props {
   type: 'email' | 'password';
   size: 'S' | 'L';
-  onSubmit: (value: string) => void;
   placeholder?: string;
-  inputname?: string;
+  inputname: string;
 }
 
-const Input: React.FC<Props> = ({ type, size, onSubmit, placeholder = '입력하시오.', inputname = '' }) => {
+const Input: React.FC<Props> = ({ type, size, placeholder = '입력하시오.', inputname = '입력하시오.' }) => {
   const [inputvalue, setInputvalue] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSubmit(inputvalue);
-  };
-
   return (
-    <form className="form" data-size={size} data-type={type} onSubmit={handleSubmit}>
-      <label className="inputname" data-size={size}>
-        {inputname}
-      </label>
+    <form className="form" data-size={size} data-type={type}>
+      {inputname != '' && (
+        <label className="inputname" data-size={size}>
+          {inputname}
+        </label>
+      )}
       <input className="input" data-size={size} type={type} placeholder={placeholder} value={inputvalue} onChange={(e) => setInputvalue(e.target.value)} />
     </form>
   );
