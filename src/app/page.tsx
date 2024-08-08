2bcf5e6e775340be6b0b06d241ea3b0afe23acad
cossgii <eastar385@naver.com>
2024-08-08 21:02:49 +0900

    fix: 인풋 수정 찐최종

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-/*export default function Home() {
-  return <div></div>;
-}
-*/
-'use client';
-import React from 'react';
-import Image from 'next/image';
-//import styles from './page.module.css';
-import Input from '@/components/inputcomponent/Input';
-
 export default function Home() {
-  return (
-    <>
-      <Input size="S" placeholder="Enter your email address" inputname="이메일" />
-    </>
-  );
+  return <div></div>;
 }
--- src/components/carddetail/Card.tsx
+++ src/components/carddetail/Card.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Card';
+import imageProp from '@/types/Image';
 
 interface Props {
   size: 'S' | 'M' | 'L';
-  image: string;
+  image: imageProp;
   wineName: string;
   wineDesc: string;
-  winePrice: string;
+  winePrice: number;
   childeren?: React.ReactNode;
 }
 //size가 S이면 className이 wine인 div태그를 제거되도록 했습니다.
@@ -14,7 +15,7 @@ const Card: React.FC<Props> = ({ size, image, wineName, wineDesc, winePrice }) =
   return (
     <div className="hiddenCard">
       <div className="card" data-size={size}>
-        <img src={image} alt="Card Image" className="cardImage" data-size={size} />
+        <img src={image ? image : ''} alt="Card Image" className="cardImage" data-size={size} />
         {size !== 'S' ? (
           <div className="wine">
             <div className="wineName" data-size={size}>
