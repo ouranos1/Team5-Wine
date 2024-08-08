7f2578c971c884077f31e031eb40ab69838132fe
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-08 18:43:30 +0900

    사용방법 업데이트

--- src/app/winedetail/page.tsx
+++ src/app/winedetail/page.tsx
@@ -1,64 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import Cardmonthly from '../../components/cardmonthly/Card';
-import Cardmy from '../../components/cardmy/Card';
-import { wineListAPI, wineDetail } from "@/api/Wine";
-import { winListType, wineDetailType } from "@/types/WineProps";
 
 
-const App: React.FC = () => {
-    const [wineList, setWineList] = useState<winListType[]>([]);
-    const [detail, setDetail] = useState<wineDetailType>();
+return (
+    <div>
 
-    useEffect(() => {
-        const fetchWineList = async () => {
-            try {
-                const response = await wineListAPI(999999);
-                setWineList(response.list);
-                console.log(response.list);
-            } catch (error) {
-                console.error("Error fetching wine list:", error);
-            }
-        };
-
-        const fetchWineMy = async () => {
-            try {
-                const response = await wineDetail(35);
-                setDetail(response);
-                console.log(response);
-            } catch (error) {
-                console.error("Error fetching wine list:", error);
-            }
-        };
-
-
-
-        fetchWineList();
-        fetchWineMy();
-    }, []);
-
-    //const whiteWineList = wineList.filter(wine => wine.type === 'WHITE');
-
-    return (
-        <div>
-            {wineList.map((wine) => (
-                <Cardmonthly key={wine.id}
-                    image={wine.image}
-                    avgRating={wine.avgRating}
-                    name={wine.name}
-                    size="L" />
-            ))}
-            {detail !== undefined ? detail.reviews.map((d) => (
-                <Cardmy key={d.id}
-                    rating={d.rating}
-                    createdAt={d.createdAt}
-                    name={detail.name}
-                    content={d.content}
-                    size="L" />
-            )) : ""}
-        </div>
-    );
+    </div>
+);
 };
 
 export default App;
