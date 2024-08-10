7859c788e23506bc7d3d92fa92523d933bea151a
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-10 21:20:24 +0900

    모달 연결

--- src/components/ratingall/RatingAll.tsx
+++ src/components/ratingall/RatingAll.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 import './RatingAll.scss';
 import { wineDetail } from '@/api/Wine';
 import { wineDetailType } from '@/types/WineProps';
-import Button from '../button/Button';
-import RatingBar from '../ratingbar/RatingBar';
-import RatingStart from '../ratingstart/RatingStart';
+import Button from '@/components/button/Button';
+import RatingBar from '@/components/ratingbar/RatingBar';
+import RatingStart from '@/components/ratingstart/RatingStart';
+import { ModalReview } from '@/components/modal/modalreview/ModalReview';
 
 interface RatingAllProps {
     score: 1 | 2 | 3 | 4 | 5;
@@ -23,6 +24,16 @@ interface RatingAllProps {
 }
 
 const RatingAll: React.FC<RatingAllProps> = ({ score, avgRating, avgRatings, reviewCount, size }) => {
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+    const handleOpenModal = () => {
+        console.log('마침 궁금했다고')
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <>
@@ -32,7 +43,7 @@ const RatingAll: React.FC<RatingAllProps> = ({ score, avgRating, avgRatings, rev
                         <RatingStart size={size} starSize={size === 'L' ? 17 : 12} avgRating={avgRating} reviewCount={reviewCount} />
                     </div>
                     <div className='allButton'>
-                        <Button text={'리뷰 남기기'} />
+                        <Button text={'리뷰 남기기'} onClick={handleOpenModal} />
                     </div>
                 </div>
                 <div className='allRatingBars'>
@@ -43,6 +54,11 @@ const RatingAll: React.FC<RatingAllProps> = ({ score, avgRating, avgRatings, rev
                     <RatingBar score={1} avgRatings={avgRatings} reviewCount={reviewCount} />
                 </div>
             </div >
+            <ModalReview
+                isModalOpen={isModalOpen}
+                closeModal={handleCloseModal}
+                wineName="와인 이름"
+            />
         </>
     );
 };
