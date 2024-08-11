ccb39705b3ae3bd43261e5fdf3f84827e190a41a
cossgii <eastar385@naver.com>
2024-08-11 18:41:02 +0900

    feat: 카드와인

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import Image from 'next/image';
 //import styles from './page.module.css';
-import Card from '@/components/cardmylist/card';
+import Card from '@/components/cardwine/card';
 
 export default function Cardpage() {
   const wineprice = 64990;
+  const review = 'Cherry, cocoa, vanilla and clove - beautiful red fruit driven Amarone. Low acidity and medium tannins. Nice long velvety finish.';
   return (
     <>
-      <Card image="예시.png" wineName="Sentinel Carbernet Sauvignon 2016" wineDesc="Western Cape, South Africa" winePrice={wineprice} />
+      <Card image="예시.png" wineName="Sentinel Carbernet Sauvignon 2016" wineDesc="Western Cape, South Africa" winePrice={wineprice} review={review} />
     </>
   );
 }
