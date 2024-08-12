80cc48c0bac4c1e6042d21ad03c43880523f16d9
cossgii <eastar385@naver.com>
2024-08-13 07:01:37 +0900

    feat: 와인 목록 페이지 진행중

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -1,4 +1,4 @@
-/*'use client';
+'use client';
 import React from 'react';
 //import Image from 'next/image';
 import Card from '@/components/cardwine/Card';
@@ -9,19 +9,6 @@ export default function Cardpage() {
   return (
     <>
       <Card image="예시.png" wineName="Sentinel Carbernet Sauvignon 2016" wineDesc="Western Cape, South Africa" winePrice={wineprice} review={review} />
-    </>
-  );
-}*/
-'use client';
-import React from 'react';
-import Image from 'next/image';
-//import styles from './page.module.css';
-import Input from '@/components/inputComponent/Input';
-
-export default function Home() {
-  return (
-    <>
-      <Input type="email" placeholder="Enter your email address" inputname="Email" />
     </>
   );
 }
