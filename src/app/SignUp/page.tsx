29787e699cbde8c4cb9c04d561537c15c21ddf45
yeram7591 <dhflgjsxj2@naver.com>
2024-08-13 06:53:59 +0900

    feat: GNB 조건부 렌더링 구현

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -26,11 +26,11 @@ export default function SignUp() {
       {/* <SearchBar placeholder="와인을 검색해 보세요" />
       <OAuthButton logo={googleLogo} text={session ? '로그아웃' : 'google로 시작하기'} onClick={session ? signOut : () => signIn('google')} />
       <OAuthButton logo={kakaoLogo} text={session ? '로그아웃' : 'kakao로 시작하기'} onClick={session ? signOut : () => signIn('kakao')} /> */}
-      <button onClick={openModal}>모달열기</button>
+      {/* <button onClick={openModal}>모달열기</button> */}
       {/* <ModalFilter isModalOpen={isModalOpen} closeModal={closeModal} setWines={setWines} /> */}
-      {/* <ModalWine isModalOpen={isModalOpen} closeModal={closeModal} /> */}
+      <ModalWine isModalOpen={isModalOpen} closeModal={closeModal} />
       {/* <ModalEdit isModalOpen={isModalOpen} closeModal={closeModal} /> */}
-      <ModalReview isModalOpen={isModalOpen} wineId={35} closeModal={closeModal} wineName="test와인" />
+      {/* <ModalReview isModalOpen={isModalOpen} wineId={35} closeModal={closeModal} wineName="test와인" /> */}
       <SignupForm />
     </>
   );
