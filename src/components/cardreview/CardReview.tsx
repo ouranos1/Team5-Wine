32facf077350dcbbc421bfb33b4e229d6bd82150
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-12 19:06:58 +0900

    css 적용

--- src/components/cardreview/CardReview.tsx
+++ src/components/cardreview/CardReview.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './CardReview.scss';
+import Image from 'next/image';
 import { AromaTag } from '@/components/aromatag/AromaTag';
 import { Aroma, AromaName } from '@/types/Aroma';
 import { reviewDetailType } from '@/types/ReviewProps'
@@ -11,6 +12,7 @@ import { SlideMode } from '@/types/SlideOption';
 import { id } from '@/types/Id'
 import WineTasteSlide from '@/components/wineTaste/WineTasteSlide';
 import { searchReviewsAPI } from '@/api/Review'
+import defaultprofile from '@/assets/icon/defaultprofile.webp'
 
 interface ReviewProps {
     reviewId: id;
@@ -37,12 +39,15 @@ const CardReview: React.FC<ReviewProps> = ({ reviewId }) => {
             {detail &&
                 <div className="soohyun-card">
                     <div className="soohyun-header">
+                        {detail.user.image ?
+                            <img src={detail.user.image} className="soohyun-image" /> :
+                            <Image src={defaultprofile} alt="와인아이콘" className="soohyun-image" />
+                        }
                         <span className="soohyun-nickname">{detail.user.nickname}</span>
                         <span className="soohyun-date">{new Date(detail.createdAt).toISOString().split('T')[0]}</span>
                     </div>
                     <div className="soohyun-aroma">
-                        <AromaTag option="view" list={createAromaList(detail.aroma)} />
-                    </div>
+                        <AromaTag option="view" list={createAromaList(detail.aroma)} /></div>
                     <div className="soohyun-content">
                         {detail.content}
                     </div>
