ac3f62834120834855e2e7f63b636926916726c1
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-13 02:50:25 +0900

    리뷰 남기기 가능해짐

--- src/app/wines/[id]/page.tsx
+++ src/app/wines/[id]/page.tsx
@@ -7,6 +7,7 @@ import { wineDetail } from '@/api/Wine';
 import { wineDetailType } from '@/types/WineProps';
 import RatingAll from '@/components/ratingall/RatingAll';
 import CardReview from '@/components/cardreview/CardReview';
+import { ModalReview } from '@/components/modal/modalreview/ModalReview';
 
 interface PageProps {
     params: { id: string };
@@ -16,6 +17,7 @@ const App: React.FC<PageProps> = ({ params }) => {
     const id = parseInt(params.id, 10);
     const [detail, setDetail] = useState<wineDetailType | null>(null);
     const [score, setScore] = useState<1 | 2 | 3 | 4 | 5>(1);
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
 
     useEffect(() => {
         const fetchWineDetail = async () => {
@@ -28,18 +30,28 @@ const App: React.FC<PageProps> = ({ params }) => {
             }
         };
         fetchWineDetail();
-    }, [id]);
+    }, [id, isModalOpen]);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
 
     return (
         <div className='page'>
             {detail && (
                 <>
                     <Card image={detail.image} wineName={detail.name} wineDesc={detail.region} winePrice={detail.price} />
-                    <RatingAll score={score} avgRating={detail.avgRating} avgRatings={detail.avgRatings} reviewCount={detail.reviewCount} />
+                    <RatingAll score={score} avgRating={detail.avgRating} avgRatings={detail.avgRatings} reviewCount={detail.reviewCount} handleOpenModal={handleOpenModal} />
                     {detail.reviewCount > 0 ? detail.reviews.map((review) => (
                         <CardReview key={review.id} reviewId={review.id} />
                     )) :
                         <div>리뷰 없음냥</div>}
+                    <ModalReview
+                        isModalOpen={isModalOpen}
+                        closeModal={handleOpenModal}
+                        wineName="와인 이름"
+                        wineId={id}
+                    />
                 </>
             )}
         </div>
