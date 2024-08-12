968f70c19ea2fc78370e1916db014f64a5cffdc6
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-13 07:20:39 +0900

    페이지 위치 조정 중

--- src/components/modal/modalreview/ModalReview.tsx
+++ src/components/modal/modalreview/ModalReview.tsx
@@ -111,7 +111,9 @@ export function ModalReview({ isModalOpen, closeModal, wineName, wineId, ReviewD
                     <Stars stars={ReviewData.rating} isEvent={true} onRatingChange={handleRatingChange} />
                   </div>
                 </div>
-                <Input type="email" defaultValue={ReviewData.content} inputname="" onChange={(e) => setReviewContent(e.target.value)} />
+                <div className='input-width'>
+                  <Input type="email" defaultValue={ReviewData.content} inputname="" onChange={(e) => setReviewContent(e.target.value)} />
+                </div>
               </div>
             </div>
             <div className="wine-taste">
