73dd60eb567a580f815138b5cd1899a61251ea01
HanjuJung <hjvzx3502@gmail.com>
2024-08-13 23:08:17 +0900

    GNB 드롭다운 수정

--- src/components/gnb/GNB.tsx
+++ src/components/gnb/GNB.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './GNB.scss';
 import Logo from '@/assets/icon/logo_wine.svg';
 import Image from 'next/image';
@@ -16,10 +16,12 @@ const GNB: React.FC = () => {
   const userData = session.data?.user.user;
 
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null); // 드롭다운과 아이콘을 감싸는 div를 참조
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
   const pathname = usePathname(); // 현재 페이지 경로를 가져옴
   const isRoot = pathname === '/';
 
@@ -28,13 +30,27 @@ const GNB: React.FC = () => {
     { name: '로그아웃', path: '/' },
   ];
 
+  // useEffect로 전역 클릭 이벤트 감지
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false); // 드롭다운 외부를 클릭하면 닫히게 설정
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="gnb">
       <Link href="/">
         <Image src={Logo} alt="Logo" className="logo" />
       </Link>
       {userData ? (
-        <div className="gnb-right">
+        <div className="gnb-right" ref={dropdownRef}>
           <Image src={userData.image ? userData.image : defaultprofile} alt="User" className="user-image" onClick={toggleDropdown} />
           {isOpen && ( // isOpen이 true일 때만 드롭다운을 렌더링
             <div className="dropdown-container" onClick={toggleDropdown}>
