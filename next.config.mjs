e2eb64960e00e6c6d3603e53fc47c20f0a47a35a
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-08-14 06:05:17 +0900

    오류 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [ 'test.com',  'i.pinimg.com' ,'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',],
+    domains: ['test.com', 'i.pinimg.com', 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com', 't4.daumcdn.net',],
   },
 };
 
